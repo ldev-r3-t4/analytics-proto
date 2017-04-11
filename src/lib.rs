7caab5f89746b410cefd2ea22400820c0efc0162
Geordon Worley <vadixidav@gmail.com>
2017-04-10 21:03:35 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate chrono;
+
+use chrono::{UTC, DateTime};
+
+#[derive(Serialize, Deserialize)]
+enum Message {
+    AllChannels {
+        total_channels: usize,
+        success: bool,
+    },
+    MostRecentMessages { num_messages: usize, success: bool },
+    MoreMessages {
+        num_requested: usize,
+        num_sent: usize,
+        success: bool,
+    },
+    SendMessage {
+        message_length: usize,
+        success: bool,
+    },
+}
+
+#[derive(Serialize, Deserialize)]
+struct Event {
+    received_time: DateTime<UTC>,
+    serviced_time: DateTime<UTC>,
+    message: Message,
+}
+
