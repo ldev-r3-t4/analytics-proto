c63c76d51e31f323772bb08c29056b3698fcc5b4
Geordon Worley <vadixidav@gmail.com>
2017-04-15 18:56:54 -0400

    add crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! 
+//! This crate contains the API for the protocol of the analytics server. `Event` is the type which is sent to the
+//! analytics server. Here are some examples of `Event` structures using different `Message` variants:
+//!
+//! `{"received_time":"2017-04-11T04:29:16.064185621Z","serviced_time":"2017-04-11T04:29:16.064188591Z","message":{"AllChannels":{"total_channels":5,"success":true}}}`
+//!
+//! `{"received_time":"2017-04-11T04:29:16.064324152Z","serviced_time":"2017-04-11T04:29:16.064324934Z","message":{"MostRecentMessages":{"num_messages":8,"success":true}}}`
+//!
+//! `{"received_time":"2017-04-11T04:29:16.064422755Z","serviced_time":"2017-04-11T04:29:16.064423786Z","message":{"MoreMessages":{"num_requested":5,"num_sent":0,"success":false}}}`
+//!
+//! `{"received_time":"2017-04-11T04:29:16.064537018Z","serviced_time":"2017-04-11T04:29:16.064537698Z","message":{"SendMessage":{"message_length":87,"success":true}}}`
+//!
+//! `{"received_time":"2017-04-11T04:29:16.064613838Z","serviced_time":"2017-04-11T04:29:16.064614475Z","message":{"CreateChannel":{"channel_name_length":7,"success":true}}}`
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
